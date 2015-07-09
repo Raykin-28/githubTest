c5fd41cf88f5e27bf764b0de59444c99ee5130ea
Raykin <dirty_wu@163.com>
2015-07-09 19:40:04 +0800

    create hello.java

---
+++ src/raykin/git/com/Hello.java
@@ -0,0 +1,14 @@
+package raykin.git.com;
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello,world!");
+
+	}
+
+}
